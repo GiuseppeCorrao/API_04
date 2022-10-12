8818618863eddbb1cf29436c06969725b4ef378d
GiuseppeCorrao <109970421+GiuseppeCorrao@users.noreply.github.com>
2022-10-12 18:26:34 +0200

    initial commit

---
+++ API_04/API_04/src/main/java/develhope/API_04/Api04Application.java
@@ -0,0 +1,13 @@
+package develhope.API_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Api04Application.class, args);
+	}
+
+}
---
+++ API_04/API_04/src/main/java/develhope/API_04/controller/HeaderController.java
@@ -0,0 +1,16 @@
+package develhope.API_04.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HeaderController {
+
+    @GetMapping("/headers")
+    public String returnHeaders(@RequestHeader(HttpHeaders.HOST) String host) {
+
+        return host;
+    }
+}
